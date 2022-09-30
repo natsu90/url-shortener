1eb61cd2b62c1d4f944494b3087c5f79f1d2ba1f
Sulaiman Sudirman <mohd.sulaiman@sudirman.info>
2022-10-01 09:40:13 +1300

    Create data.js

---
+++ data.js
@@ -0,0 +1,16 @@
+const data = {
+  li: "linkedin.com/in/sulaiman-sudirman",
+  linkedin: "linkedin.com/in/sulaiman-sudirman",
+  gh: "github.com/natsu90",
+  github: "github.com/natsu90",
+  so: "stackoverflow.com/u/1109004",
+  stackoverflow: "stackoverflow.com/u/1109004",
+  blog: "sulaiman.dev",
+  fb: "facebook.com/mohd.sulaiman.sudirman",
+  facebook: "facebook.com/mohd.sulaiman.sudirman",
+  ig: "instagram.com/mohd.sulaiman.sudirman",
+  instagram: "facebook.com/mohd.sulaiman.sudirman",
+  goodreads: "goodreads.com/sulaiman_sudirman",
+  twitter: "twitter.com/natsu90",
+  spotify: "open.spotify.com/user/natsu901"
+}
